88770af3a6614f71962d55ea8142e85a25c9c53d
Abdur Rehman Imran <arehmanimran4@gmail.com>
2025-08-27 15:16:52 +0500

    Basic argument handling

--- ls.c
+++ ls.c
@@ -2,6 +2,8 @@
 #include <dirent.h>   // For getting list of members of a directory
 #include <string.h>   // For using functions like strcmp
 #include <sys/stat.h> // For differentiating between directories and files
+#include <stdlib.h>
+
 
 int get_no_of_files(DIR ** directory,struct dirent ** dir_entry){
   int count = 0;
@@ -61,15 +63,38 @@ int isDirectory(char * path)
   return S_ISDIR(statbuf.st_mode);
 }
 
+
+void printFiles(int no_of_files,char * files[]){
+  for (int i=0; i<no_of_files; i++){
+    printf("%s\t", files[i]);
+  }
+}
+
 int main(int argc,char* argv[])
 {
   struct dirent *dir_entry;
-  const char *dir_path =  (char *)argv[1];
+  char *dir_path; 
+  int mode = 0;
+  // Dealing with args
+  for (int i=1; i<argc; i++)
+  {
+    const char* arg = argv[i];
+    if (arg[0] != '-') {
+      dir_path = (char*)malloc(sizeof(arg)); // Allocating some memory to the pointer
+      strcpy(dir_path, arg);
+    }
+    else if (arg[0] == '-'){
+      if (strcmp("-a",arg) == 0){
+        mode = 1;
+      }
+    }
+  }
 
   // Opening a directory
   DIR * directory = opendir(dir_path);
 
   if (directory==NULL){
+    printf("Error while opening the given directory!");
     return 1;
   }
 
@@ -82,13 +107,17 @@ int main(int argc,char* argv[])
 
   char * hidden_files[num_of_hidden_files];
   get_hidden_files(count, names, hidden_files);
-  
-  for (int i=0; i<num_of_hidden_files; i++)
-  {
-    printf("%-10s", hidden_files[i]);
+  // puts("Switch");
+  switch (mode) {
+    case 0:
+      printFiles(count, names);
+      break;
+    case 1:
+      printFiles(num_of_hidden_files, hidden_files);
+      break;
+     
   }
-  
-  puts("");
+  puts(""); // Adds a new line!
 
   //Closing the opened directory
   closedir(directory);
